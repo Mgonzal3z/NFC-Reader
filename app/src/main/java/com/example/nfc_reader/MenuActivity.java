15dd9af51310eb35a4df71a4e753b1c6382a9d4c
Mgonzal3z <upgrade_tech@hotmail.com>
2023-11-25 19:39:30 -0500

    h

--- app/src/main/java/com/example/nfc_reader/MenuActivity.java
+++ app/src/main/java/com/example/nfc_reader/MenuActivity.java
@@ -2,22 +2,51 @@ package com.example.nfc_reader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
 import android.os.Bundle;
-import android.nfc.Tag;
-import android.util.Log;
-import java.io.IOException;
-import java.nio.charset.Charset;
-
+import android.os.Parcelable;
+import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
+
+    private TextView nfcDataTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-
-        
+        nfcDataTextView = findViewById(R.id.articule);
+    }
+    // Manejar la intención NFC
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+            Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+            if (rawMessages != null) {
+                NdefMessage[] messages = new NdefMessage[rawMessages.length];
+                for (int i = 0; i < rawMessages.length; i++) {
+                    messages[i] = (NdefMessage) rawMessages[i];
+                }
+                // Obtener los datos de la etiqueta y mostrarlos en el TextView
+                NdefRecord record = messages[0].getRecords()[0];
+                String nfcData = new String(record.getPayload());
+                nfcDataTextView.setText(nfcData);
+            }
+        }
     }
 
+    // Deshabilitar el despacho en primer plano cuando la actividad está en pausa
+    @Override
+    protected void onPause() {
+        super.onPause();
+        NfcAdapter nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        if (nfcAdapter != null) {
+            nfcAdapter.disableForegroundDispatch(this);
+        }
+    }
 }
